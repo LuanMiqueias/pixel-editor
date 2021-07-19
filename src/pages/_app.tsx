4cc4fadeeab19c6287a289d97648132c49631d2d
Luan Miqueias <luanmiqueias92@gmail.com>
2021-07-19 14:26:18 -0300

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+      </Head>
       <a
         href="https://github.com/LuanMiqueias/pixel-editor"
         target="_blank"
